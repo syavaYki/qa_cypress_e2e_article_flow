e502bc6cd09948100b2132f59a9f1653db44f724
syavaYki <s_podolskiy@live.com>
2025-07-23 13:07:40 -0400

    solution fix

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,29 +1,27 @@
 // @ts-nocheck
 describe('Test Article Flow', () => {
-  let user;
-  let article;
-
   beforeEach(() => {
-    cy.task('generateUser').then((generatedUser) => {
-      user = generatedUser;
-
-      cy.login(user.email, user.username, user.password).then(() => {
-        cy.task('generateArticle').then((generatedArticle) => {
-          article = generatedArticle;
+    cy.task('generateUser')
+      .then((user) => {
+        cy.wrap(user).as('user');
+        cy.login(user.email, user.username, user.password);
+      })
+      .then(() => {
+        cy.task('generateArticle').then((article) => {
+          cy.wrap(article).as('article');
           cy.createArticle(article.title, article.description, article.body);
         });
       });
-    });
   });
 
-  it('should create an article and see it in profile', () => {
-    cy.visit(`/profile/${user.username.toLowerCase()}`);
-    cy.contains('h1', article.title).should('be.visible');
+  it('should create an article and see it in profile', function () {
+    cy.visit(`/profile/${this.user.username.toLowerCase()}`);
+    cy.contains('h1', this.article.title).should('be.visible');
   });
 
-  it('should delete the article', () => {
-    cy.visit(`/profile/${user.username.toLowerCase()}`);
-    cy.contains('h1', article.title).click();
+  it('should delete the article', function () {
+    cy.visit(`/profile/${this.user.username.toLowerCase()}`);
+    cy.contains('h1', this.article.title).click();
     cy.contains('button', 'Delete Article').click();
     cy.contains('.article-preview', 'No articles are here... yet.').should(
       'be.visible'
