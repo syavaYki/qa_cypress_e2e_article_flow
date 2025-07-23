0d6caf7ab181213d0325cdf4a6e706312b0872e5
syavaYki <s_podolskiy@live.com>
2025-07-23 12:56:23 -0400

    solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,32 @@
-describe('', () => {
-  before(() => {
+// @ts-nocheck
+describe('Test Article Flow', () => {
+  let user;
+  let article;
 
+  beforeEach(() => {
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+
+      cy.login(user.email, user.username, user.password).then(() => {
+        cy.task('generateArticle').then((generatedArticle) => {
+          article = generatedArticle;
+          cy.createArticle(article.title, article.description, article.body);
+        });
+      });
+    });
   });
 
-  it('', () => {
-
+  it('should create an article and see it in profile', () => {
+    cy.visit(`/profile/${user.username.toLowerCase()}`);
+    cy.contains('h1', article.title).should('be.visible');
   });
 
-  it('', () => {
-
+  it('should delete the article', () => {
+    cy.visit(`/profile/${user.username.toLowerCase()}`);
+    cy.contains('h1', article.title).click();
+    cy.contains('button', 'Delete Article').click();
+    cy.contains('.article-preview', 'No articles are here... yet.').should(
+      'be.visible'
+    );
   });
 });
